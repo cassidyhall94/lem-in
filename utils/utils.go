7a1e213c892a93ca21b4222d8ec9e9f7a1886d36
jagodasad <jago.sadowska@gmail.com>
2022-03-22 13:20:41 +0000

    paths sorted by length

--- utils/utils.go
+++ utils/utils.go
@@ -12,13 +12,27 @@ func CheckError(err error) {
 	}
 }
 
+// Sort paths by length
+func SortPathsByLength(paths *[][]*structs.Room) {
+	l := len(*paths)
+	for i := 0; i < l; i++ {
+		min := i
+		for j := i; j < l; j++ {
+			if len((*paths)[j]) < len((*paths)[min]) {
+				min = j
+			}
+		}
+		(*paths)[i], (*paths)[min] = (*paths)[min], (*paths)[i]
+	}
+}
+
 // Decodes paths list into room names
 func DecodePaths(paths [][]*structs.Room) {
 	fmt.Println()
-	for b := 0; b < len(paths); b++ {
-		for c := 0; c < len(paths[b]); c++ {
-			fmt.Print(paths[b][c].Name)
-			if c != len(paths[b])-1 {
+	for i := 0; i < len(paths); i++ {
+		for j := 0; j < len(paths[i]); j++ {
+			fmt.Print(paths[i][j].Name)
+			if j != len(paths[i])-1 {
 				fmt.Print(" -> ")
 			} else {
 				fmt.Println()
