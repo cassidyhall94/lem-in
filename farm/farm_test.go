c42a2e499b759fe8a0a1b6f25c044dd56380c902
Cassidy.Hall94 <cassidy.hall94@gmail.com>
2022-03-24 16:34:53 +0000

    thinking about stuff

--- farm/farm_test.go
+++ farm/farm_test.go
@@ -18,6 +18,7 @@ func TestGenerateFarm(t *testing.T) {
 		{
 			name: "pass",
 			args: args{
+				// In this struct, room 0 is the start and room 1 is the end
 				data: structs.GenerationData{
 					NumberOfAnts: 3,
 					Rooms:        []string{"1", "0"},
@@ -25,15 +26,28 @@ func TestGenerateFarm(t *testing.T) {
 					StartIndex:   0,
 					EndIndex:     1,
 				},
+				// In this struct, room bar is the start and room foo is the end
+				// data: structs.GenerationData{
+				// 	NumberOfAnts: 3,
+				// 	Rooms:        []string{"foo", "zip", "bar"},
+				// 	Links:        []string{"foo-zip", "zip-bar"},
+				// 	StartIndex:   1,
+				// 	EndIndex:     0,
+				// },
 			},
 			want: structs.Farm{
+				// What about the Ants: []*Ant?
 				Rooms: []*structs.Room{
+					// How many rooms do you need to define?
 					{
-						Name:  "0",
-						Ants:  3,
-						X_pos: 0,
-						// Y_pos:   1,
+						Name: "0",
+						// How many ants can a room have? What is the significance of this value?
+						Ants:    3,
+						X_pos:   0,
+						Y_pos:   1,
+						// Is the room defined here the start room?
 						IsStart: true,
+						// What about it being the end room?
 						IsEnd:   false,
 						// Links: []*structs.Room {
 
@@ -46,6 +60,7 @@ func TestGenerateFarm(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			// This function, right now, doesn't generate links so your test should assume that none exist
 			if got := GenerateFarm(tt.args.data); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("GenerateFarm() = %+v, want %+v", got, tt.want)
 			}
