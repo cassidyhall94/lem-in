84bf65f6f569b59ad1277c55e874cd87b2bdf31a
cassidy.hall94 <cassidy.hall94@gmail.com>
2022-05-05 17:43:01 +0100

    ants formatting

--- README.md
+++
@@ -1,7 +0,0 @@
-First row sets amount of ants
-A room is defined by "name coord_x and coord_y" (For example "Room 1 2", "nameoftheroom 1 6", "467")
-Links are defined by "name1-name2" (For example "1-2", "2-8") -##start assigns room as start and ##end assigns room as end -#comment just adds comment to the text file
-Examples: cat maps/example01.txt go run . maps/example01.txt
-
-https://go.dev/blog/go1.13-errors
-https://github.com/golang-standards/project-layout
--- internal/ants/ants.go
+++ internal/ants/ants.go
@@ -7,10 +7,6 @@ import (
 )
 
 // func CreateAnts assigns a path to each ant
-// (ants []*structs.Ant) is the same thing as (ants structs.farm) from the farm function
-// ants will move through the shortest path, we can use current room and rooms passed to measure their movement
-// loop through all the ants and move them a room, one ant at a time
-// store information in variable to be printed at a later time
 
 // Lx-y Lz-w Lr-o ...
 //     x, z, r represents the ants numbers (going from 1 to number_of_ants) and y, w, o represents the rooms names.
@@ -18,87 +14,51 @@ import (
 //     The coordinates of the rooms are only important for the Bonus task: ant farm visualizer.
 //     The links (tunnels) are defined by "name1-name2" and will usually look like "1-2", "2-5".
 //     The L is for Lem-in.
-// L1-2
-// L1-3 L2-2
-// L1-1 L2-3 L3-2
-// L2-1 L3-3
-// L3-1
+
+// store everything to one string and then range over the string to print slices
+// use roomsPassed to add conditional statements for formatting?
+// fmt.Printf("oneAntStruct: %+v\n, CurrentRoom: %+v\n", oneAntStruct, oneAntStruct.CurrentRoom)
+// fmt.Printf("Ant ID: %+v CurrentRoom: %+v RoomsPassed: %+v\n", oneAntStruct.Id, oneAntStruct.CurrentRoom.Name, oneAntStruct.RoomsPassed)
 func MoveAnts(ants []*structs.Ant, path *structs.PathStruct) []string {
 	result := []string{}
 	for _, oneAntStruct := range ants {
-		// oneAntStruct: &{Id:1 CurrentRoom:<nil> RoomsPassed:0}
 		for _, oneRoom := range path.Path {
-			// oneRoom: &{Name:0 Ants:0 IsStart:true IsEnd:false Links:[]}
 			if oneRoom.IsStart || oneRoom.IsEnd {
 				oneRoom.Ants = len(ants)
-				if !oneRoom.IsStart {
+				if oneRoom.IsEnd {
 					oneAntStruct.RoomsPassed += 1
 				}
 				oneAntStruct.CurrentRoom = oneRoom
-				// START: oneAntStruct.CurrentRoom: &{Name:0 Ants:3 IsStart:true IsEnd:false Links:[]}
-				// END: oneAntStruct.CurrentRoom: &{Name:1 Ants:3 IsStart:false IsEnd:true Links:[]}
-
-				// fmt.Printf("oneAntStruct.CurrentRoom: %+v\n", oneAntStruct.CurrentRoom)
+				if oneRoom.IsEnd {
+					endPrint := fmt.Sprintf("L%v-%v", oneAntStruct.Id, oneAntStruct.CurrentRoom.Name)
+					result = append(result, endPrint)
+				}
 			} else if !oneRoom.IsStart && !oneRoom.IsEnd {
 				oneAntStruct.RoomsPassed += 1
 				oneRoom.Ants = +1
 				oneAntStruct.CurrentRoom = oneRoom
-				// oneAntStruct.CurrentRoom: &{Name:3 Ants:1 IsStart:false IsEnd:false Links:[]}
-				// oneAntStruct.CurrentRoom: &{Name:2 Ants:1 IsStart:false IsEnd:false Links:[]}
-
-				// fmt.Printf("oneAntStruct.CurrentRoom: %+v\n", oneAntStruct.CurrentRoom)
-				// fmt.Printf("oneAntStruct %+v\n", oneAntStruct)
+				roomPrint := fmt.Sprintf("L%v-%v", oneAntStruct.Id, oneAntStruct.CurrentRoom.Name)
+				result = append(result, roomPrint)
 			}
-			fmt.Printf("oneAntStruct: %+v\n, CurrentRoom: %+v\n", oneAntStruct, oneAntStruct.CurrentRoom)
 		}
+		for _, resultString := range result {
+			if oneAntStruct.RoomsPassed <= i {
+			}
+		}
+		// fmt.Printf("result: %+v\n", result)
+
 	}
+
 	return result
 }
 
-// func lenPathStruct(paths []*structs.PathStruct) int {
-// 	lenPathCounter := 0
-
-// 	for _, path := range paths {
-// 		for _, pathRoom := range path.Path {
-// 			if pathRoom.Name != "" {
-// 				lenPathCounter = lenPathCounter + 1
-// 			}
+// func sliceOfStrings(result []string) []string {
+// 	oneAntStruct := &structs.Ant{}
+// 	for i, resultString := range result {
+// 		if oneAntStruct.RoomsPassed <= i {
+// 			resultString = fmt.Sprintf("L%v-%v", oneAntStruct.Id, oneAntStruct.CurrentRoom.Name)
+// 			result = append(result, resultString)
 // 		}
 // 	}
-// 	return lenPathCounter
-// }
-
-// func diffRoomsInPaths(paths []*structs.PathStruct) []int {
-// 	difPaths := []int{}
-// 	noOfRoomsPath := []int{}
-
-// 	for n := 0; n < lenPathStruct(paths); n++ {
-// 		noOfRooms := len(paths[n].Path)
-// 		noOfRoomsPath = append(noOfRoomsPath, noOfRooms)
-// 	}
-
-// 	for d := 1; d < len(noOfRoomsPath); d++ {
-// 		difRooms := noOfRoomsPath[d] - noOfRoomsPath[d-1]
-// 		difPaths = append(difPaths, difRooms)
-// 	}
-
-// 	return difPaths
-// }
-
-// func antsPerPath(paths []*structs.PathStruct) []int {
-// 	noOfPaths := lenPathStruct(paths)
-// 	difPaths := diffRoomsInPaths(paths)
-// 	antsPerPath := []int{}
-
-// 	antsPerPath = append(antsPerPath, 20/noOfPaths+difPaths[0]-1)
-// 	for dr := 1; dr < noOfPaths; dr++ {
-// 		antCounter := 20 - antsPerPath[dr-1]
-// 		if dr == len(difPaths) {
-// 			antsPerPath = append(antsPerPath, antCounter/len(difPaths))
-// 		} else {
-// 			antsPerPath = append(antsPerPath, antCounter/len(difPaths)+difPaths[dr]-1)
-// 		}
-// 	}
-
-// 	return antsPerPath
+// 	return result
 // }
